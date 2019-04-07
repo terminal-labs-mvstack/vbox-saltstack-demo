cc81f2bc1ada810c5a65bae413e130b03735543f
Michael Verhulst <michael@terminallabs.com>
2019-04-06 22:24:41 -0500

    Update and rename install_lxc_before_reboot.sh to install_lxc.sh

--- minion/install_lxc.sh
+++ minion/install_lxc.sh
@@ -10,7 +10,4 @@ yum-config-manager --disable zfs
 yum-config-manager --enable zfs-kmod
 yum -y install zfs
 
-# Configure the kernel
-grubby --args="user_namespace.enable=1" --update-kernel="$(grubby --default-kernel)"
-grubby --args="namespace.unpriv_enable=1" --update-kernel="$(grubby --default-kernel)"
-echo "user.max_user_namespaces=3883" > /etc/sysctl.d/99-userns.conf
+snap install lxd
